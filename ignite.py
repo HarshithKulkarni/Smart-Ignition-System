b409ff288bb7bd25b23999278866b9c21eb0e100
Harshith Kulkarni <harshithkulkarni@gmail.com>
2020-02-05 23:11:22 +0530

    Create ignite.py

---
+++ ignite.py
@@ -0,0 +1,75 @@
+import face_recognition
+import cv2
+import numpy as np
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BOARD)
+relay = 31
+gpio.setup(relay,gpio.OUT)
+
+video_capture = cv2.VideoCapture(0)
+
+HK_image = face_recognition.load_image_file("HK.jpg")
+HK_face_encoding = face_recognition.face_encodings(HK_image)[0]
+known_face_encodings = [HK_face_encoding]
+known_face_names = ["HK"]
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+flag = 0
+
+while True:
+    ret, frame = video_capture.read()
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                flag = 1
+            else:
+                flag = 0
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow('Video', frame)
+    print(flag)
+    if(flag==1):
+        gpio.output(relay,gpio.HIGH)
+        time.sleep(1)
+        gpio.output(relay,gpio.LOW)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
